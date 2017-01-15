36cfc9c895aa9078bb49ceec03ff318414477a49
alone <saeed.kamranfar@gmail.com>
2017-01-16 01:33:01 +0330

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+from pylab import *
+import matplotlib.pyplot as plt
+from pylab import *
+import numpy as np
+theta = np.arange(-np.pi, np.pi, .01)[1:] 
+
+def f(x):
+    return x
+
+plt.polar(theta, abs(sin(np.pi/2)*cos(theta)))
+show()
